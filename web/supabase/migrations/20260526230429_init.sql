6f77404d22cda16a957534db3c0897efb85cd91e
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-05-28 10:08:30 +0200

    fix: remove redundant storage policies from init migration

--- web/supabase/migrations/20260526230429_init.sql
+++ web/supabase/migrations/20260526230429_init.sql
@@ -201,39 +201,3 @@ with check ((auth.uid() = id));
   for select
   to authenticated
 using ((auth.uid() = id));
-
-
-
-  create policy "Give users access to own folder 1m0cqf_0"
-  on "storage"."objects"
-  as permissive
-  for select
-  to public
-using (((bucket_id = 'files'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Give users access to own folder 1m0cqf_1"
-  on "storage"."objects"
-  as permissive
-  for insert
-  to public
-with check (((bucket_id = 'files'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Give users access to own folder 1m0cqf_2"
-  on "storage"."objects"
-  as permissive
-  for update
-  to public
-using (((bucket_id = 'files'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Give users access to own folder 1m0cqf_3"
-  on "storage"."objects"
-  as permissive
-  for delete
-  to public
-using (((bucket_id = 'files'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
